646b560d36cd1b87150b718e0dbab697cb8dfe79
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2024-01-24 18:31:15 +0530

    category routing complete

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { authenticate, authorizedAdmin } from "../middleware/authMiddleware.js";
-import { createCategory, updateCategory } from "../controllers/categoryController.js";
+import { createCategory, updateCategory, removeCategory, listCategory, readCategory } from "../controllers/categoryController.js";
 
 const router = express.Router()
 
 router.route('/').post(authenticate,authorizedAdmin,createCategory);
 router.route('/:categoryId').put(authenticate,authorizedAdmin, updateCategory)
+router.route('/:categoryId').delete(authenticate, authorizedAdmin, removeCategory)
+router.route('/categories').get(listCategory)
+router.route('/:id').get(readCategory)
 
 export default router;

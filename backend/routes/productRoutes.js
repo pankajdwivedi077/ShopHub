138f2273b16d31a888f4170d8e76f387907d11fd
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2024-01-25 20:07:11 +0530

    productController

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,13 +1,26 @@
 import express from "express";
-import formidable from 'express-formidable'
+import formidable from "express-formidable";
 import { authenticate, authorizedAdmin } from "../middleware/authMiddleware.js";
 import checkId from "../middleware/checkId.js";
-import { addProduct } from "../controllers/productController.js";
+import {
+  addProduct,
+  updateProductDetails,
+  removeProduct,
+  fetchProducts,
+  fetchProductById,
+  fetchAllProducts,
+  addProductReview
+} from "../controllers/productController.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.route('/').post(authenticate,authorizedAdmin, formidable(), addProduct)
-
-
+router.route("/").get(fetchProducts).post(authenticate, authorizedAdmin, formidable(), addProduct);
+router.route('/allProducts').get(fetchAllProducts);
+router.route('/:id/reviews').post(authenticate, authorizedAdmin, addProductReview)
+router
+  .route("/:id")
+  .get(fetchProductById)
+  .put(authenticate, authorizedAdmin, formidable(), updateProductDetails)
+  .delete(authenticate, authorizedAdmin, removeProduct)
 
 export default router;

346126363b872ce9719b470984ab17165c9f02d4
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2024-01-27 18:40:22 +0530

    ProductList jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -18,6 +18,7 @@ import Login from './pages/Auth/Login.jsx';
 import Register from './pages/Auth/Register.jsx';
 import UserList from './pages/Admin/UserList.jsx';
 import CategoryList from './pages/Admin/CategoryList.jsx';
+import ProductList from './pages/Admin/ProductList.jsx';
 
 
 const router = createBrowserRouter(
@@ -35,6 +36,7 @@ const router = createBrowserRouter(
       <Route path='/admin' element={<AdminRoute />}>
         <Route path='userlist' element={<UserList />} />
         <Route path='categorylist' element={<CategoryList />} />
+        <Route path='productlist' element={<ProductList />} />
       </Route>
 
     </Route>

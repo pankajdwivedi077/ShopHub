0eb13ac65cc8ef220002d74f52e4a501bd9b1329
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2024-01-27 19:27:47 +0530

    Product upload

--- frontend/src/pages/Admin/ProductList.jsx
+++ frontend/src/pages/Admin/ProductList.jsx
@@ -1,12 +1,12 @@
-import React from "react";
 import { useState } from "react";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 import {
   useCreateProductMutation,
   useUploadProductImageMutation,
 } from "../../redux/api/productApiSlice";
 import { useFetchCategoriesQuery } from "../../redux/api/categoryApiSlice";
 import { toast } from "react-toastify";
+
 
 const ProductList = () => {
   const [image, setImage] = useState("");
@@ -18,12 +18,53 @@ const ProductList = () => {
   const [brand, setBrand] = useState("");
   const [stock, setStock] = useState(0);
   const [imageUrl, setImageUrl] = useState(null);
-
   const navigate = useNavigate();
 
   const [uploadProductImage] = useUploadProductImageMutation();
   const [createProduct] = useCreateProductMutation();
   const { data: categories } = useFetchCategoriesQuery();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const productData = new FormData();
+      productData.append("image", image);
+      productData.append("name", name);
+      productData.append("description", description);
+      productData.append("price", price);
+      productData.append("category", category);
+      productData.append("quantity", quantity);
+      productData.append("brand", brand);
+      productData.append("countInStock", stock);
+
+      const { data } = await createProduct(productData);
+
+      if (data.error) {
+        toast.error("Product create failed. Try Again.");
+      } else {
+        toast.success(`${data.name} is created`);
+        navigate("/");
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("Product create failed. Try Again.");
+    }
+  };
+
+  const uploadFileHandler = async (e) => {
+    const formData = new FormData();
+    formData.append("image", e.target.files[0]);
+
+    try {
+      const res = await uploadProductImage(formData).unwrap();
+      toast.success(res.message);
+      setImage(res.image);
+      setImageUrl(res.image);
+    } catch (error) {
+      toast.error(error?.data?.message || error.error);
+    }
+  };
 
   return (
     <div className="container xl:mx-[9rem] sm:mx-[0]">
@@ -50,8 +91,8 @@ const ProductList = () => {
                 type="file"
                 name="image"
                 accept="image/*"
-                // onChange={uploadFileHandler}
-                className={!image ? "hidden" : "text-black"}
+                onChange={uploadFileHandler}
+                className={!image ? "hidden" : "text-white"}
               />
             </label>
           </div>
@@ -108,21 +149,39 @@ const ProductList = () => {
               onChange={(e) => setDescription(e.target.value)}
             ></textarea>
 
-          <div className="flex justify-between">
-            <div>
+            <div className="flex justify-between">
+              <div>
                 <label htmlFor="name block">Count In Stock</label> <br />
-                <input type="text" className="p-4 mb-3 w-[25rem] border rounded-lg bg-[white] text-black " value={stock} onChange={(e) => setStock(e.target.value)}/>
-            </div>
+                <input
+                  type="text"
+                  className="p-4 mb-3 w-[25rem] border rounded-lg bg-[white] text-black"
+                  value={stock}
+                  onChange={(e) => setStock(e.target.value)}
+                />
+              </div>
 
-            <div>
-                <label htmlFor="">Catagory</label> <br />
-                <select placeHolder="Choose Catagory" className="p-4 mb-3 w-[25rem] border rounded-lg bg-[white] text-black " onChange={e => setCategory(e.target.value)}>
-                    {/* {categories} */}
+              <div>
+                <label htmlFor="">Category</label> <br />
+                <select
+                  placeholder="Choose Category"
+                  className="p-4 mb-3 w-[25rem] border rounded-lg bg-[white] text-black"
+                  onChange={(e) => setCategory(e.target.value)}
+                >
+                  {categories?.map((c) => (
+                    <option key={c._id} value={c._id}>
+                      {c.name}
+                    </option>
+                  ))}
                 </select>
+              </div>
             </div>
 
-          </div>
-            
+            <button
+              onClick={handleSubmit}
+              className="py-4 px-10 mt-5 rounded-lg text-lg font-bold bg-pink-600"
+            >
+              Submit
+            </button>
           </div>
         </div>
       </div>
